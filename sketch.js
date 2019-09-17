03c2b796bdf8fa51fad53853bec1a09c7087f9f9
Maataro <masahiro.infinite77@gmail.com>
2019-09-18 06:07:43 +0900

    first commit

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 
 let mobilenet; // Classifier
-let puffin;
+let targetImg; // Image from user
+let dropzone;  // Place to drop image
+
+let labelP;
+let confidenceP;
+
 
 function modelReady() {
   console.log('Model is ready!!!');
-  mobilenet.predict(puffin, gotResults); 
+  mobilenet.predict(targetImg, gotResults);
 }
 
 function gotResults(error, results) {
@@ -14,25 +19,63 @@ function gotResults(error, results) {
     console.log(results);
     let label = results[0].label;
     let confidence = results[0].confidence;
+
     fill(0);
-    textSize(64);
+    rect(0, height - 50, width, 50);
+    fill(255);
+    textSize(36);
     text(label, 10, height - 10);
-    createP(label);
-    createP(confidence);
+
+    labelP.html("Label: " + label);
+    let confidencePercent = nf(confidence * 100.0, 0, 2);
+    confidenceP.html("Confidence: " + confidencePercent + "%");
   }
 }
 
 function imageReady() {
-  image(puffin, 0, 0, width, height);
+  image(targetImg, 0, 0, width, height);
+
 }
 
 function setup() {
- createCanvas(640, 480);
+  createCanvas(640, 550);
 
- puffin = createImg('images/tosainu.jpg', imageReady);  
- puffin.hide();  // Canvas上にのみイメージを配置したい。
- background(0);
+  background(0);
+  labelP = createP("Label: ");
+  labelP.id('label');
+  confidenceP = createP("Confidence: ");
+  confidenceP.id('label');
 
- // generate Image classification object (model name, callback function)
- mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+  dropzone = select('#dropzone');
+  dropzone.dragOver(highlight);
+  dropzone.dragLeave(unhighlight);
+  dropzone.drop(gotFile, unhighlight); // (handlefile, dropevent)
+}
+
+function draw() {
+  // fill(0);
+  // rect(0, height - 20, width, 20);
+  // fill(255);
+  // textSize(64);
+  // text(label, 10, height - 10);
+
+}
+
+function gotFile(file) {
+  //createP(file.name + " " + file.size);
+  //createP(file.type);
+
+  targetImg = createImg(file.data, imageReady);
+  targetImg.hide(); // Canvas上にのみイメージを配置したい。
+
+  // generate Image classification object (model name, callback function)
+  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+}
+
+function highlight() {
+  dropzone.style('background-color', '#ccc');
+}
+
+function unhighlight() {
+  dropzone.style('background-color', '#fff');
 }
